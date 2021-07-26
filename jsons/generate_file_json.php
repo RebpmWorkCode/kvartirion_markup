79d0f0b41d995dde894c8c538b8404163a5d05ed
vadimushka_d <vadimushka.d@gmail.com>
2021-07-26 13:20:09 +0300

    fixed jsons

--- jsons/generate_file_json.php
+++ jsons/generate_file_json.php
@@ -14,8 +14,9 @@ $sorted = [
     'Regions_twig_realty_index.json',
     //twig_realty_view
     'Regions_twig_realty_main.json',
-    //twig_realty_map
+    'Regions_twig_realty_map.json',
     'Regions_twig_realty_form.json',
+    'Regions_twig_realty_category_params.json',
     'Regions_twig_favorites_index.json',
     'Regions_twig_feedback_send.json',
     //twig_nodes_index
@@ -38,9 +39,31 @@ foreach ($jsons as $jsonFile) {
     }
     $sorted[] = $jsonFile;
 }
+
+$processFiles = function ($content) {
+    $region = $content[0]['Region'];
+    $blocks = $content[0]['Block'];
+    $region = array_intersect_key($region, ['title' => true, 'alias' => true, 'block_count' => true]);
+    $region['title'] = trim($region['title']);
+    $region['alias'] = trim($region['alias']);
+    $region['block_count'] = count($blocks);
+    array_walk($blocks, function (&$block) {
+        $block = array_intersect_key($block, ['title' => true, 'alias' => true, 'body' => true, 'status' => true, 'params' => true]);
+        $block['title'] = trim($block['title']);
+        $block['alias'] = trim($block['alias']);
+        $block['status'] = true;
+        $block['params'] = 'disabledCkEditor=1';
+    });
+    $content[0]['Region'] = $region;
+    $content[0]['Block'] = $blocks;
+    return $content;
+};
+
 foreach ($sorted as $fileName) {
     $content = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . $fileName);
-    $content = json_decode($content, true)[0];
-    $regions[] = $content;
+    $content = json_decode($content, true);
+    $content = $processFiles($content);
+    file_put_contents(__DIR__ . DIRECTORY_SEPARATOR . $fileName, json_encode($content, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+    $regions[] = $content[0];
 }
 file_put_contents(__DIR__ . DIRECTORY_SEPARATOR . $generatedFileName, json_encode($regions, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
